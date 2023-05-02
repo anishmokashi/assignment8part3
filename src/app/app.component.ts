bd8570d8eae072034a704c78022fc1a640859500
unknown <anish.mokashi13@gmail.com>
2023-05-02 23:39:44 +0530

    ninth commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'assignment8part3';
+  username="";
+  //username1=this.username.length;
+}
